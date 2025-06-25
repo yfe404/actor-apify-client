84dea9f79620d4ae9d1247240d6d1c0cf57cb6b9
yfe404 <yfe.github@protonmail.com>
2025-06-25 14:23:32 +0200

    run actor using apifyClient

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { Actor } from 'apify';
 import { ApifyClient } from 'apify-client';
 
-
 interface Input {
-    token: string;
+    token: string,
+    actorId: string,
+    input: string
 }
 await Actor.init();
 
-const input = await Actor.getInput<Input>();
-if (!input) throw new Error('Input is missing!');
-const { token } = input;
+const { token, actorId, input } = inputData;
 
-if (!token) {
-    throw new Error('Please provide a `datasetId` in the input.');
-}
+// call actorId with input
+const apifyClient = Actor.newClient();
+console.log(`Calling actor ${actorId}`);
+await Actor.call(actorId, JSON.parse(input));
+
+// retrieve datasets from actor runs
+
+// save dataset as CSV under OUTPUT.csv
 
 await Actor.exit();
