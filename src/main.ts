f9d5bfef643737e3a9e999af68aec685f9ad156b
yfe404 <yfe.github@protonmail.com>
2025-06-30 11:00:40 +0200

    feat(package.json): add apify-client dependency to enable client functionality chore(main.ts): remove unused imports and commented code for cleaner codebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { Actor, Dataset } from 'apify';
-import { ApifyClient } from 'apify-client';
 
 interface Input {
     actorId: string;
@@ -9,17 +8,11 @@ await Actor.init();
 
 const { actorId, input } = (await Actor.getInput<Input>())!;
 
-// call actorId with input
-const apifyClient = Actor.newClient();
 console.log(`Calling actor ${actorId}`);
-//await Actor.call(actorId, input);
 const datasetId = (await Actor.call(actorId, input)).defaultDatasetId;
 console.log(`Actor run completed. Dataset ID: ${datasetId}`);
 
-//const dataset = await apifyClient.datasets.getDataset({ datasetId });
-// Open a named dataset
 const dataset = await Dataset.open(datasetId);
-// Convert dataset items to CSV
 await dataset.exportToCSV('OUTPUT.csv');
 
 const keyValueStoreId = Actor.getEnv().defaultKeyValueStoreId;
